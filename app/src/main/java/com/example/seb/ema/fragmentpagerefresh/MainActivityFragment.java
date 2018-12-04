d5fa094c24e257e62953d098b501d7fa24c569b2
seb <inf2947@hs-worms.de>
2018-12-04 18:28:03 +0100

    add logout add back button in Fragments

--- app/src/main/java/com/example/seb/ema/fragmentpagerefresh/MainActivityFragment.java
+++ app/src/main/java/com/example/seb/ema/fragmentpagerefresh/MainActivityFragment.java
@@ -1,9 +1,11 @@
 package com.example.seb.ema.fragmentpagerefresh;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 
+import com.example.seb.ema.Main2Activity;
 import com.example.seb.ema.R;
 import com.example.seb.ema.framents.FragmentMain;
 
@@ -36,7 +38,13 @@ public class MainActivityFragment extends FragmentActivity {
     @Override
     public void onAttachFragment(Fragment fragment) {
         super.onAttachFragment(fragment);
-
     }
 
+    @Override
+    public void onBackPressed() {
+        // do something on back.
+        Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
+        startActivity(intent);
+        finish();
+    }
 }
